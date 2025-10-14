94af07593a33658f9e8c35b175e6448edca94ade
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-14 18:24:20 +0900

    feat : 연산기능 추가 fix : try , catch 구문으로 에러시 출력을 주었는데 b = scanner.nextInt();으로 할당하는 부분이 괄호에 들어가 있어서 이후 연산에서 재사용불가했음 -> scanner.hasNextInt()사용 후 에러부분에서 scanner.next()로 값버림

--- src/level1/CalculatorBasic.java
+++ src/level1/CalculatorBasic.java
@@ -10,38 +10,40 @@ public class CalculatorBasic {
         /*첫번째 숫자*/
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         Scanner scanner = new Scanner(System.in);
-//
+
         int a;
-
-        try {
-            System.out.print("첫 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
-            a = scanner.nextInt();
-
-            /*음수 입력시 재입력 요청*/
-            while (a < 0) {
-                System.out.println("0 또는 양의 정수를 입력해주세요 : ");
+        System.out.print("첫 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
+        while (true) {
+            if (scanner.hasNextInt()) {
                 a = scanner.nextInt();
+                if (a >= 0) {
+                    break; /*올바른 입력 -> 반복 종료*/
+                } else {
+                    System.out.print("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
+                }
+            } else {
+                System.out.print("**에러: 0 또는 양의 정수를 입력해야합니다");
+                scanner.next(); /*잘못된 입력 제거*/
             }
-            /*문자, 실수 입력으로 에러나면 다시 알려주기*/
-        } catch (Exception e) {
-            System.out.println("**에러: 0 또는 양의 정수를 입력해야합니다");
         }
+
+
 
         /*두번째 숫자*/
         int b;
-
-        try {
-            System.out.print("두 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
-            b = scanner.nextInt();
-
-            /*음수 입력시 재입력 요청*/
-            while (!(b >= 0)) {
-                System.out.println("0 또는 양의 정수를 입력해주세요 : ");
+        System.out.print("첫 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
+        while (true) {
+            if (scanner.hasNextInt()) {
                 b = scanner.nextInt();
+                if (b >= 0) {
+                    break; /*올바른 입력 -> 반복 종료*/
+                } else {
+                    System.out.print("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
+                }
+            } else {
+                System.out.print("**에러: 0 또는 양의 정수를 입력해야합니다");
+                scanner.next(); /*잘못된 입력 제거*/
             }
-            /*문자, 실수 입력으로 에러나면 다시 알려주기*/
-        } catch (Exception e) {
-            System.out.println("**에러: 0 또는 양의 정수를 입력해야합니다");
         }
 
         /*사칙연산*/
@@ -55,6 +57,30 @@ public class CalculatorBasic {
             System.out.println(" +, -, /, * 로 사칙연산을 입력해주세요 : ");
             op = scanner.next().charAt(0);
         }
+
+        /* 연산 */
+        double result = 0;
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    System.out.println("**에러: 0으로 나눌 수 없습니다!");
+                    return;
+                }
+                result = (double) a / (double) b;
+                break;
+        }
+        System.out.println("결과: " + a + " " + op + " " + b + " = " + result);
+
+
     }
 
 
