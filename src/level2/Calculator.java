f82a1f9936164764d94b7f8ed4e03e788173ad4a
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-15 22:00:45 +0900

    fix : resultList.add(result); 에서 캐스팅(int) 없어진채 (double)로 저장되고 출력되는 문제 -> 출력시 DecimalFormat적용하고 캐스팅은삭제 style : 사칙연산 순서변경, print 구문 println으로 변경 refactor : level2에서는 클래스 설정이 되므로 반복문 긴 거 모두 없애고 c.calculator를 실행하는 것으로 변경

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -2,11 +2,12 @@ package level2;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Calculator {
 
     // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-    Scanner scanner = new Scanner(System.in);
+    public Scanner scanner = new Scanner(System.in);
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
     private ArrayList<Double> resultList = new ArrayList<>();
 
@@ -21,7 +22,7 @@ public class Calculator {
     public double calculator(int a, int b, char op) {
 
         /*첫번째 숫자*/
-        System.out.print("첫 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
+        System.out.println("첫 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
 
         while (true) {
             if (scanner.hasNextInt()) {
@@ -29,27 +30,10 @@ public class Calculator {
                 if (a >= 0) {
                     break; /*올바른 입력 -> 반복 종료*/
                 } else {
-                    System.out.print("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
+                    System.out.println("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
                 }
             } else {
-                System.out.print("**에러: 0 또는 양의 정수를 입력해야합니다");
-                scanner.next(); /*잘못된 입력 제거*/
-            }
-        }
-
-        /*두번째 숫자*/
-        System.out.print("두 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
-
-        while (true) {
-            if (scanner.hasNextInt()) {
-                b = scanner.nextInt();
-                if (b >= 0) {
-                    break; /*올바른 입력 -> 반복 종료*/
-                } else {
-                    System.out.print("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
-                }
-            } else {
-                System.out.print("**에러: 0 또는 양의 정수를 입력해야합니다");
+                System.out.println("**에러: 0 또는 양의 정수를 입력해야합니다");
                 scanner.next(); /*잘못된 입력 제거*/
             }
         }
@@ -57,7 +41,7 @@ public class Calculator {
         /*사칙연산*/
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
         /*사칙연산기호 : foreBasicOperation*/
-        System.out.print("사칙연산 기호를 입력하세요: ");
+        System.out.println("사칙연산 기호를 입력하세요: ");
 
         op = scanner.next().charAt(0);
         /*다른 한 글자 입력시 재입력 요청*/
@@ -65,6 +49,24 @@ public class Calculator {
             System.out.println(" +, -, /, * 로 사칙연산을 입력해주세요 : ");
             op = scanner.next().charAt(0);
         }
+
+        /*두번째 숫자*/
+        System.out.println("두 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                b = scanner.nextInt();
+                if (b >= 0) {
+                    break; /*올바른 입력 -> 반복 종료*/
+                } else {
+                    System.out.println("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
+                }
+            } else {
+                System.out.println("**에러: 0 또는 양의 정수를 입력해야합니다");
+                scanner.next(); /*잘못된 입력 제거*/
+            }
+        }
+
         /* 연산 */
 
         double result = 0;
@@ -85,16 +87,18 @@ public class Calculator {
                 result = (double) a / (double) b;
                 break;
         }
-        if (op == '/') {
-            if ((double) a % (double) b == 0) { /* 2/2처럼 나누어떨어질 때 1.0으로 표기되는 문제해결*/
-                System.out.println("결과: " + a + " " + op + " " + b + " = " + (int) result);
-            } else {
-                System.out.println("결과: " + a + " " + op + " " + b + " = " + result);
-            }
-        } else { /* 정수 계산에 소수점.0으로 표기되는 문제해결*/
-            System.out.println("결과: " + a + " " + op + " " + b + " = " + (int) result);
-        }
+        /* 2/2처럼 나누어떨어질 때 1.0으로 표기되는 문제해결*/
+        /* 정수 계산에 소수점.0으로 표기되는 문제해결*/
+        DecimalFormat df = new DecimalFormat("#.#################");
+        System.out.println("결과: " + a + " " + op + " " + b + " = " + df.format(result));
         resultList.add(result);
+
+        System.out.print("결과값 리스트 : ");
+        for (double r : resultList) {
+        System.out.print("[" + df.format(r) + "] ");
+        }
+        System.out.println();
+
         return result;
     }
 
