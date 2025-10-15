b6647464035beb6b0ef2efeb5a1e0926db3a5822
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-15 21:59:53 +0900

    fix : resultList.add(result); 에서 캐스팅(int) 없어진채 (double)로 저장되고 출력되는 문제 -> 출력시 DecimalFormat적용하고 캐스팅은삭제 style : 사칙연산 순서변경, print 구문 println으로 변경 refactor : level2에서는 클래스 설정이 되므로 반복문 긴 거 모두 없애고 c.calculator를 실행하는 것으로 변경

--- src/level1/App.java
+++ src/level1/App.java
@@ -1,19 +1,44 @@
 package level1;
 
 import level2.Calculator;
+
 import java.util.Scanner;
 
 public class App {
 
-
-    // main() — 프로그램 시작점
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         // 생성자 : Calculator 객체 생성
-            Calculator c = new Calculator();
-            // 컴파일 에러로 임의로 초기값 입력
-            c.calculator(0, 0, ' ');
+        Calculator c = new Calculator();
+        // 컴파일 에러로 임의로 초기값 입력
+        c.calculator(0, 0, ' ');
+
+
+        /*반복문을 사용하되, 반복의 종료를 알려주는 'N' 문자를 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기*/
+
+        char repeat;
+
+        System.out.println("더 계산하시겠습니까? (Y / N)");
+
+        repeat = c.scanner.next().charAt(0);
+        /*다른 글자 입력시 재입력 요청*/
+        while (!(repeat == ('Y') || repeat == ('y') || repeat == ('N') || repeat == ('n'))) {
+            System.out.println("Y 또는 N으로 입력해주세요. \n더 계산하시겠습니까? (Y / N)");
+            repeat = c.scanner.next().charAt(0);
+        }
+        if (repeat == 'N' || repeat == ('n')) {
+            c.scanner.close();
+            return; /*작업종료*/
+        } else {
+            while (repeat == 'Y' || repeat == ('y')) {
+                c.calculator(0, 0, ' ');
+                System.out.println("더 계산하시겠습니까? (Y / N)");
+                repeat = c.scanner.next().charAt(0);
+            }
+
         }
 
     }
+
+}
 
