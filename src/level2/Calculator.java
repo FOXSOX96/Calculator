c7a13e5426cd2310e9ade5a768a7de9e95a0fccf
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-15 19:18:28 +0900

    feat : 계산기능을 Calculator 클래스로 옮기고 생성자로 가져오게함. 이 Calculator 클래스에 Arraylist 컬렉션기능 추가 refactor : 리턴 값이 실수와 정수로 나누어져 컬렉션에 저장하기 어려움 -> 정수 리턴을 (int)return으로 형변환하여 변수는 return으로 통일 (더했을때 소수점 표기하기 싫어서 이전에 fix해뒀었음) fix : 나누어 떨어질 때 1이 아니라 1.0으로 표기됨 -> 나누어떨어질 때 (int)return으로 형변환

---
+++ src/level2/Calculator.java
@@ -0,0 +1,105 @@
+package level2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Calculator {
+
+    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+    Scanner scanner = new Scanner(System.in);
+    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    private ArrayList<Double> resultList = new ArrayList<>();
+
+    /**
+     * 두 개의 양의 정수(또는 0)를 입력받아 사칙연산(+, -, *, /)을 수행하는 메서드
+     * @param a : 첫 번째 양의 정수(또는 0)
+     * @param b : 두 번째 양의 정수(또는 0)
+     * @param op : 사칙연산 (operation)
+     * @return result: 입력시 결과값 생성
+     */
+    /*양의 정수(0 포함)를 입력받기*/
+    public double calculator(int a, int b, char op) {
+
+        /*첫번째 숫자*/
+        System.out.print("첫 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                a = scanner.nextInt();
+                if (a >= 0) {
+                    break; /*올바른 입력 -> 반복 종료*/
+                } else {
+                    System.out.print("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
+                }
+            } else {
+                System.out.print("**에러: 0 또는 양의 정수를 입력해야합니다");
+                scanner.next(); /*잘못된 입력 제거*/
+            }
+        }
+
+        /*두번째 숫자*/
+        System.out.print("두 번째 숫자를 입력하세요 (0 또는 양의 정수) : ");
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                b = scanner.nextInt();
+                if (b >= 0) {
+                    break; /*올바른 입력 -> 반복 종료*/
+                } else {
+                    System.out.print("0 또는 양의 정수를 입력해주세요 : "); /*음수 입력시 재입력 요청*/
+                }
+            } else {
+                System.out.print("**에러: 0 또는 양의 정수를 입력해야합니다");
+                scanner.next(); /*잘못된 입력 제거*/
+            }
+        }
+
+        /*사칙연산*/
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        /*사칙연산기호 : foreBasicOperation*/
+        System.out.print("사칙연산 기호를 입력하세요: ");
+
+        op = scanner.next().charAt(0);
+        /*다른 한 글자 입력시 재입력 요청*/
+        while (!(op == ('+') || op == ('-') || op == ('*') || op == ('/'))) {
+            System.out.println(" +, -, /, * 로 사칙연산을 입력해주세요 : ");
+            op = scanner.next().charAt(0);
+        }
+        /* 연산 */
+
+        double result = 0;
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    System.out.println("**에러: 0으로 나눌 수 없습니다!");
+                }
+                result = (double) a / (double) b;
+                break;
+        }
+        if (op == '/') {
+            if ((double) a % (double) b == 0) { /* 2/2처럼 나누어떨어질 때 1.0으로 표기되는 문제해결*/
+                System.out.println("결과: " + a + " " + op + " " + b + " = " + (int) result);
+            } else {
+                System.out.println("결과: " + a + " " + op + " " + b + " = " + result);
+            }
+        } else { /* 정수 계산에 소수점.0으로 표기되는 문제해결*/
+            System.out.println("결과: " + a + " " + op + " " + b + " = " + (int) result);
+        }
+        resultList.add(result);
+        return result;
+    }
+
+
+}
+
+
+
